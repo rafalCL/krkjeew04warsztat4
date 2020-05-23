19128ab39daeda364bcdb9ba1b5f4ad75f84f660
Rafal <rafal.opial@coderslab.pl>
2020-05-23 14:42:10 +0200

    Display book list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 $(function(){
-    console.log("działa");
+    var bookListDiv = $(".book-list");
+    
+    refreshBookList();
+    
+    function refreshBookList(){
+        renderBookList(bookListDiv);
+    }
+    
+    function renderBookList(renderingPoint){
+        $.ajax({
+            url : "http://localhost:8282/books",
+            type : "GET",
+            dataType : "json",
+        }).done(function(booksArr){
+            renderingPoint.empty();
+            
+            for(var i=0; i<booksArr.length; i++){
+                var titleDiv = $('<div class="book-title">');
+                titleDiv.text(booksArr[i].title);
+                
+                renderingPoint.append(titleDiv);
+            }
+        }).fail(function(xhr, status, err){
+            console.log(xhr, status, err);
+        });
+    }
 })
